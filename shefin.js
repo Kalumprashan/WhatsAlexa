8eca505d94aacb52583eb73c5ccb5429a4afcd28
SHEFIN KL14 <93778980+shefinkl14@users.noreply.github.com>
2022-02-12 22:39:24 +0530

    WhatsAlexa First Release ✅️

--- shefin.js
+++ shefin.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
- SN14: process.env.BAD_WORDS === undefined ? 'false' : process.env.BAD_WORDS,
+ SN14: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
  Bot_Name: process.env.Bot_Name === undefined ? 'WhatsAlexa' : process.env.Bot_Name,
  Bot: process.env.Bot === undefined ? 'https://i.ibb.co/KGMms2Z/Whats-Alexa.jpg' : process.env.Bot,
     
